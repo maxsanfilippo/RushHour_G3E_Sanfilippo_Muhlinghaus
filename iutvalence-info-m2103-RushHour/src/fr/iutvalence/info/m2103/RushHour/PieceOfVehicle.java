efd44855da25e2b2cba5c9a6a2d6515ee01c99cf
Maxx <Maxx@Maxx-PC>
2015-05-17 15:18:34 +0200

    Complete class vehicle
    Constructor and method go

---
+++ iutvalence-info-m2103-RushHour/src/fr/iutvalence/info/m2103/RushHour/PieceOfVehicle.java
@@ -0,0 +1,10 @@
+package fr.iutvalence.info.m2103.RushHour;
+
+public class PieceOfVehicle {
+
+	public void move(boolean forwardMovement, boolean horizontalMovement) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- iutvalence-info-m2103-RushHour/src/fr/iutvalence/info/m2103/RushHour/Vehicle.java
+++ iutvalence-info-m2103-RushHour/src/fr/iutvalence/info/m2103/RushHour/Vehicle.java
@@ -1,5 +1,25 @@
 package fr.iutvalence.info.m2103.RushHour;
 
-public class Vehicle {
+import java.util.*;
 
+public class Vehicle 
+{
+	private List<PieceOfVehicle> pieceList;
+	private boolean horizontalMovement;
+	
+	public Vehicle(boolean horizontalMovement)
+	{
+		this.horizontalMovement = horizontalMovement;
+		this.pieceList = new ArrayList<PieceOfVehicle>();
+	}
+	public void go(boolean forwardMovement)
+	{
+		for (Iterator<PieceOfVehicle> iterator = pieceList.iterator(); iterator.hasNext();) 
+			//parcours de la liste avec iterator
+		{
+			PieceOfVehicle pieceOfVehicle = (PieceOfVehicle) iterator.next();
+			pieceOfVehicle.move(forwardMovement, this.horizontalMovement);
+			// on déplace le véhicule en déplacant ses pièces
+		}
+	}
 }
